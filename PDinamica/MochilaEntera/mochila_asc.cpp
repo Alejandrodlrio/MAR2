f7e2b32cc0c3378516d9c6bb2050363a5e32c4e1
Alejandro del Río <58173617+Alejandrodlrio@users.noreply.github.com>
2026-03-16 14:46:18 +0100

    mochila ascendente corregida

--- PDinamica/MochilaEntera/mochila_asc.cpp
+++ PDinamica/MochilaEntera/mochila_asc.cpp
@@ -18,9 +18,9 @@ struct pack{
 
 
 //reconstruccion de la solucion: 
-vector<int> reconstruccion(Matriz<int> const&m, vector<pack> const& v, int n, int t){
+vector<bool> reconstruccion(Matriz<int> const&m, vector<pack> const& v, int n, int t){
 
-    vector<int> x;
+    vector<bool> x(n, false);
     //hay solucion
     if(m[n][t] != -1){
 
@@ -28,9 +28,9 @@ vector<int> reconstruccion(Matriz<int> const&m, vector<pack> const& v, int n, in
         int j = t;
     
         //"Mientras aun quede espacio"
-        while(j > 0 && i > 0){
+        while(j > 0 && i > 0) {
             if(m[i][j] != m[i - 1][j]){
-                x.push_back(i - 1);
+                x[i - 1] = true;
                 j = j - v[i - 1].p;
             }
             --i;
@@ -43,19 +43,21 @@ vector<int> reconstruccion(Matriz<int> const&m, vector<pack> const& v, int n, in
 
 
 //Devuelve el valor maximo con x objetos.
-int mochila(Matriz<int> matriz, vector<pack> const& v, int n, int t){
+int mochila(Matriz<int> &matriz, vector<pack> const& v, int n, int t){
 
-    matriz[0][0] = 0;
 
-    for(int i = 1; i <= n; ++i){
+    for(int i = 0; i <= n; ++i){
         matriz[i][0] = 0;
         for(int j = 1; j <= t; ++j){
-            if(v[i - 1].p > j) {
-                //copiamos lo obtenido hasta ahora
-                matriz[i][j] = matriz[i - 1][j];
-            }
+            if(i == 0){ matriz[i][j] = 0;}
             else{
-                matriz[i][j] = max(matriz[i - 1][j], matriz[i - 1][j - v[i - 1].p] + v[i-1].v);
+                if(v[i - 1].p > j) {
+                    //copiamos lo obtenido hasta ahora
+                    matriz[i][j] = matriz[i - 1][j];
+                }
+                else{
+                    matriz[i][j] = max(matriz[i - 1][j], matriz[i - 1][j - v[i - 1].p] + v[i-1].v);
+                }
             }
         }
 
@@ -64,10 +66,7 @@ int mochila(Matriz<int> matriz, vector<pack> const& v, int n, int t){
 
 }
 
-// mochila_optimizada: 
-// int mochila_optimizada(vector<int> matriz, vector<pack> const&v, int n , int t){
-//     for(int i = 1; i <= t; ++)
-// }
+
 void resuelveCaso(){
     int n, m; cin >> n >> m;
     
@@ -78,8 +77,9 @@ void resuelveCaso(){
 
     int r = mochila(matriz, x, n, m);
     cout << "Valor maximo obtenido: " << r << endl;
-    vector<int> v = reconstruccion(matriz, x, n, m);
-    cout << "reconstruccion de la solucion: " << v.size();
+    vector<bool> v = reconstruccion(matriz, x, n, m);
+    cout << "reconstruccion de la solucion: " ; 
+    for(auto i: v) cout << i << " ";
 }
 
 
